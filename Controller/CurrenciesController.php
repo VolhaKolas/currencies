bebd42b996b809a4c374bd32cb8b74147f777b93
Volha Kolas <olgakolos1411@gmail.com>
2017-11-04 00:13:49 +0300

    2 commit

--- Controller/CurrenciesController.php
+++ Controller/CurrenciesController.php
@@ -7,6 +7,7 @@
  */
 namespace Controller;
 use Core\Database;
+use Exception;
 class CurrenciesController
 {
     public function get() {
@@ -36,37 +37,47 @@ class CurrenciesController
                     }
                 }
                 $url = "http://www.nbrb.by/API/ExRates/Rates?onDate=" . $request . "&Periodicity=0"; //url of Nat. Bank
-                $currencies = file_get_contents($url);
-                $currencies = json_decode($currencies);
-                $date = $currencies[0]->Date;
-                $dateCountSql = "SELECT COUNT(*) count FROM date WHERE d_date = \"$date\"";// check DB on existence of input date
-                $dateCount = Database::getInstance()->select($dateCountSql)[0]['count'];
-                if (0 == $dateCount) { //if there isn't input date in DB, enter it
-                    Database::getInstance()->insert('date', [ //вводим дату в базу
-                        'd_id' => null,
-                        'd_date' => $date
-                    ]);
-                    $dateIdSql = "SELECT d_id FROM date WHERE d_date = \"$date\"";
-                    $dateId = Database::getInstance()->select($dateIdSql)[0]['d_id']; //id of input date
-                    $currenciesIdsSql = "SELECT c_id, c_name FROM currencies";
-                    $currenciesIds = Database::getInstance()->select($currenciesIdsSql);//ids of currencies names
-                    foreach ($currencies as $currency) {
-                        foreach ($currenciesIds as $currenciesId) {
-                            if ($currency->Cur_Abbreviation == $currenciesId['c_name']) {//if abbreviation from Nat.Bank url == currency name
-                                Database::getInstance()->insert('m2m_date_currencies', [// add Rates to DB on input date
-                                    'dc_id' => null,
-                                    'd_id' => $dateId,
-                                    'c_id' => $currenciesId['c_id'],
-                                    'dc_value' => $currency->Cur_OfficialRate,
-                                    'dc_scale' => $currency->Cur_Scale
-                                ]);
+                try {
+                    $Headers = @get_headers($url);
+                    if(preg_match("|200|", $Headers[0])) { //написать проверку на существование url
+                        $currencies = file_get_contents($url);
+                        $currencies = json_decode($currencies);
+                        $date = $currencies[0]->Date;
+                        $dateCountSql = "SELECT COUNT(*) count FROM date WHERE d_date = \"$date\"";// check DB on existence of input date
+                        $dateCount = Database::getInstance()->select($dateCountSql)[0]['count'];
+                        if (0 == $dateCount) { //if there isn't input date in DB, enter it
+                            Database::getInstance()->insert('date', [ //вводим дату в базу
+                                'd_id' => null,
+                                'd_date' => $date
+                            ]);
+                            $dateIdSql = "SELECT d_id FROM date WHERE d_date = \"$date\"";
+                            $dateId = Database::getInstance()->select($dateIdSql)[0]['d_id']; //id of input date
+                            $currenciesIdsSql = "SELECT c_id, c_name FROM currencies";
+                            $currenciesIds = Database::getInstance()->select($currenciesIdsSql);//ids of currencies names
+                            foreach ($currencies as $currency) {
+                                foreach ($currenciesIds as $currenciesId) {
+                                    if ($currency->Cur_Abbreviation == $currenciesId['c_name']) {//if abbreviation from Nat.Bank url == currency name
+                                        Database::getInstance()->insert('m2m_date_currencies', [// add Rates to DB on input date
+                                            'dc_id' => null,
+                                            'd_id' => $dateId,
+                                            'c_id' => $currenciesId['c_id'],
+                                            'dc_value' => $currency->Cur_OfficialRate,
+                                            'dc_scale' => $currency->Cur_Scale
+                                        ]);
+                                    }
+                                }
                             }
+                            $_SESSION['message'] = "Данные успешно добавлены";
+                        } else {
+                            $_SESSION['message'] = "Данная дата уже есть в базе";
                         }
                     }
-                    $_SESSION['message'] = "Данные успешно добавлены";
+                    else {
+                        throw new Exception('Нет связи с сайтом нац.банка');
+                    }
                 }
-                else {
-                    $_SESSION['message'] = "Данная дата уже есть в базе";
+                catch (\Exception $e) {
+                    $_SESSION['message'] = "Произошла ошибка: " . $e->getMessage();
                 }
             }
             else {
